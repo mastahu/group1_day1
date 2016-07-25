fd9450c336a555124152b73f1a9a3f030d235fae
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-25 11:56:36 +0200

    Variable defined

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int result = 2 + 2;
+
         System.out.println("Hi!");
+        System.out.println("Your result is : " + result );
+
 
     }
 }
