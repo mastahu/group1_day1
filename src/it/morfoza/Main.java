9bd38ce143adc32a0ace7700fef51ceed132592f
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-25 11:43:59 +0200

    Much nicer hello

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj ziom!");
+        System.out.println("Witajcie petardy i ziomeczki!");
 
     }
 }
