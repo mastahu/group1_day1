6205f1eeab797065af8d0f720e5618edd22815de
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-25 16:46:46 +0200

    Web app created

---
+++ src/it/morfoza/CalculatorWeb.java
@@ -0,0 +1,28 @@
+package it.morfoza;
+
+import spark.Spark;
+
+public class CalculatorWeb {
+
+    public static void main(String[] args) {
+
+        Spark.get("/calculator", (request, response) -> {
+            return "<html>Hi <b>buddy</b>! </html>";
+        });
+
+
+        Spark.get("/contact", ((request, response) -> {
+         return "<html>" +
+                 "<form action=\"/calculator\">" +
+                 "<input name=\"number1\">" +
+                 "<input name=\"number2\">" +
+                 "<input type=\"submit\">" +
+                 "</form>" +
+                 "</html>";
+        }));
+
+    }
+
+
+
+}
