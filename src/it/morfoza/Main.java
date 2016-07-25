96ebc4fe2dd2c868af5fb708c0fbb4c0b15affa4
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-25 14:34:12 +0200

    Scanner added for user input

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -1,13 +1,20 @@
 package it.morfoza;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int result = 2 + 2;
+        int result = 2 * 2;
 
-        System.out.println("Hi!");
-        System.out.println("Your result is : " + result );
+        System.out.println("Hi! What's your name:");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+        System.out.println(name  + ", your result is : " + result );
 
 
     }
