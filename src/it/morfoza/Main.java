b9ba7179c97f1fc7bff50a3d86d4be27f3a370b4
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-25 15:59:26 +0200

    operations: + - / * implemented

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -7,14 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int result = 2 * 2;
-
-        System.out.println("Hi! What's your name:");
-
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        System.out.println("Name operation: +-/*");
+        String operation = input.nextLine();
 
-        System.out.println(name  + ", your result is : " + result );
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        int result = 0;
+        if (operation.equals("+")) {
+            result = number1 + number2;
+        }
+        if (operation.equals("-")) {
+            result = number1 - number2;
+        }
+        if (operation.equals("*")) {
+            result = number1 * number2;
+        }
+        if (operation.equals("/")) {
+            result = number1 / number2;
+        }
+
+        System.out.println("Your result is : " + result );
 
 
     }
